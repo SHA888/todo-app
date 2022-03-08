a318dc7a019ba56b5ab264299501e6aa461c2690
Kresna Sucandra <kresnasucandra@gmail.com>
2022-03-08 13:27:21 +0800

    Feat: Added instruction in JS

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function newItem(item) {
+  // 1. Adding a new item to the list of items
+  // 2. Crossing out an item from the list items
+  // 3. Adding the delete buton "X"
+  // 4. Adding CLASS DELETE (DISPLAY: NONE) from CSS
+  // 5. Reordering the items
+}
